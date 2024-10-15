159882a1b9b0737a7addd4a8c27b139bd6c9f9a1
Chima Ilo <techtalkwithafo@gmail.com>
2024-10-15 06:33:40 +0100

    (de)activate user

---
+++ components/admin/actions/activate.vue
@@ -0,0 +1,53 @@
+<template>
+  <ui-dropdown-menu>
+    <ui-dropdown-menu-trigger as-child>
+      <ui-button class="gap-2 capitalize">
+        <Check size="16" />
+        Activate/Deactivate
+      </ui-button>
+    </ui-dropdown-menu-trigger>
+    <ui-dropdown-menu-content class="w-56">
+      <ui-dropdown-menu-item as-child class="py-0">
+        <ui-button variant="ghost" size="sm" class="gap-2 w-full font-normal rounded-none justify-start px-3"
+          @click="openActivateModal = true">
+          <Check size="16" stroke-width="4" color="green" />
+          Activate
+        </ui-button>
+      </ui-dropdown-menu-item>
+      <ui-dropdown-menu-item as-child class="py-0">
+        <ui-button variant="ghost" size="sm" class="gap-2 w-full font-normal rounded-none justify-start px-3"
+          @click="openDeactivateModal = true">
+          <X size="16" stroke-width="4" color="red" />
+          Deactivate
+        </ui-button>
+      </ui-dropdown-menu-item>
+    </ui-dropdown-menu-content>
+  </ui-dropdown-menu>
+  <admin-dialogs-activate :open="openActivateModal" title="Activate this User?"
+    message="Activating this user means the user will have access to IXL portal."
+    @update:open="openActivateModal = false" @activateUser="handleActivateUser" />
+  <admin-dialogs-deactivate :open="openDeactivateModal" title="Deactivate this User?"
+    message="Deactivating this user means the user wont be able to access IXL portal."
+    @update:open="openDeactivateModal = false" @deactivateUser="handleDeactivateUser" />
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import { Check, X } from 'lucide-vue-next'
+
+const menu = ref();
+const openActivateModal = ref(false);
+const openDeactivateModal = ref(false);
+
+const handleActivateUser = async (payload: string | string[]) => {
+  console.log('handleActivateUser', payload)
+  // TODO: user activation
+  openActivateModal.value = false
+}
+
+const handleDeactivateUser = async (payload: string | string[]) => {
+  console.log('handleDeactivateUser', payload)
+  // TODO: user deactivation
+  openDeactivateModal.value = false
+}
+</script>
---
+++ components/admin/dialogs/deactivate.vue
@@ -0,0 +1,39 @@
+<template>
+  <ui-dialog v-model:open="open" class="rounded-2xl">
+    <ui-dialog-content class="max-w-md">
+      <span class="rounded-full bg-destructive/30 inline-flex justify-center items-center h-12 w-12 text-destructive">
+        <X :size="24" stroke-width="4" />
+      </span>
+      <ui-dialog-header>
+        <ui-dialog-title>{{ props.title }}</ui-dialog-title>
+        <ui-dialog-description>
+          {{ props.message }}
+        </ui-dialog-description>
+      </ui-dialog-header>
+      <ui-dialog-footer>
+        <ui-dialog-close as-child>
+          <ui-button type="button" variant="outline">Cancel</ui-button>
+        </ui-dialog-close>
+        <ui-button type="button" variant="destructive" @click="$emit('deactivateUser')">
+          <X :size="18" />
+          Deactivate
+        </ui-button>
+      </ui-dialog-footer>
+    </ui-dialog-content>
+  </ui-dialog>
+</template>
+
+<script setup lang="ts">
+import { X } from 'lucide-vue-next'
+
+const props = defineProps({
+  title: {
+    type: String,
+    default: ''
+  },
+  message: {
+    type: String,
+    default: ''
+  }
+})
+</script>
